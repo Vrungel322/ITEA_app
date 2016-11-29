68fac0c296703fc179122560cb7dfef4d9b3f8ec
Nikita Romanenko <nikita241296@gmail.com>
2016-11-29 20:53:03 +0200

    custom View first try

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -2,56 +2,16 @@ package com.nanddgroup.itea_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private Button bTest;
+    private CustomUserInfoView cuivTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout llParentContaiter = new LinearLayout(getApplicationContext());
-        llParentContaiter.setOrientation(LinearLayout.VERTICAL);
-
-        Button b1 = new Button(getApplicationContext());
-        b1.setText("First Button");
-        b1.setTextSize(15);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        Button b2 = new Button(this);
-        b2.setText("Second Button");
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        CheckBox cb = new CheckBox(this);
-        cb.setText("First CheckBox");
-        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
-            }
-        });
-
-        llParentContaiter.addView(b1);
-        llParentContaiter.addView(b2);
-        llParentContaiter.addView(cb);
-
-
-        setContentView(llParentContaiter);
+        cuivTextView = (CustomUserInfoView) findViewById(R.id.cuivTextView);
 
     }
 }
