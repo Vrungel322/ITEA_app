72372977a6b28e7ec5baaf03fdb9a03d506d00c9
Nikita Romanenko <nikita241296@gmail.com>
2016-11-29 16:59:28 +0200

    clean project

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -1,29 +1,13 @@
 package com.nanddgroup.itea_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button bGitTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bGitTest = (Button) findViewById(R.id.bGitTest);
-
-        bGitTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Git button clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //Some add
-        //Some add 2
     }
 }
