6c7acedbf1dbdb879b60b5f03432c4af133c564b
Nikita Romanenko <nikita241296@gmail.com>
2016-12-01 17:55:09 +0200

    make branch empty

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private CustomUserInfoView cuivTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        cuivTextView = (CustomUserInfoView) findViewById(R.id.cuivTextView);
 
     }
 }
