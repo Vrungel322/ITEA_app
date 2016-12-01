ca01569e6b6f633fafef1b98118866e49d69a473
Nikita Romanenko <nikita241296@gmail.com>
2016-12-01 19:05:52 +0200

    activity lifecycle

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.nanddgroup.itea_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.wtf("lc : ", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.wtf("lc : ", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.wtf("lc : ", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.wtf("lc : ", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.wtf("lc : ", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf("lc : ", "onDestroy");
     }
 }
