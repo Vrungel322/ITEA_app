256d478bf2d45295099e0e25723b027b084a8682
Nikita Romanenko <nikita241296@gmail.com>
2016-12-01 19:36:52 +0200

    start SecondActivity

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -1,45 +1,27 @@
 package com.nanddgroup.itea_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.wtf("lc : ", "onCreate");
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.wtf("lc : ", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.wtf("lc : ", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.wtf("lc : ", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.wtf("lc : ", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.wtf("lc : ", "onDestroy");
+        bSecondActivity = (Button) findViewById(R.id.bSecondActivity);
+        bSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
