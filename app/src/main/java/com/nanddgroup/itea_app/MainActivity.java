5b05949d71a51158f7ad27850dca32650dab9196
Nikita Romanenko <nikita241296@gmail.com>
2016-12-08 20:57:51 +0200

    toolbar games

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.nanddgroup.itea_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tbMyToolbar);
+        setSupportActionBar(toolbar);
+
+//        toolbar.setBackground(null);
+        toolbar.setTitle("MyTitle");
+        toolbar.setNavigationIcon(android.R.drawable.sym_def_app_icon);
     }
+    
 }
