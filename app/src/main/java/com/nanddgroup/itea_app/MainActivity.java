3d6cbe77197cb42283c1a998116d89992b829db9
Nikita Romanenko <nikita241296@gmail.com>
2016-12-08 12:44:37 +0200

    alert dialog tested

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -1,7 +1,13 @@
 package com.nanddgroup.itea_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bAlertDialog)
+    public void bAlertDialogClick(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setCancelable(true)
+                .setPositiveButton("Positive", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Positive button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Negative button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Neutral button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setIcon(R.drawable.error_dialog)
+                .setTitle("Test Alert dialog")
+                .setView(R.layout.test_view_alert_dialog)
+                .setMessage("My message")
+                .create().show();
     }
 }
