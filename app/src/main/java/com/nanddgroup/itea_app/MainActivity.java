2a0f7328c7c964c3f7a71bd1405dbdcd8527a509
Nikita Romanenko <nikita241296@gmail.com>
2016-12-15 20:59:33 +0200

    Alert dialog

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.nanddgroup.itea_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlertDialog.Builder adBuilder = new AlertDialog.Builder(this);
+        adBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Positive button Clicked", Toast.LENGTH_SHORT).show();
+                     }
+        })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Negative button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNeutralButton("Maybe", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Maybe buttom Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        .setTitle("Test title")
+        .setIcon(R.drawable.contact_icon);
+        adBuilder.create().show();
+
     }
 }
