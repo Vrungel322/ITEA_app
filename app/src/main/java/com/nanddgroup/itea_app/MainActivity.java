ee7f5ac793b60aec14975f978c3e1c2c2076ea93
Nikita Romanenko <nikita241296@gmail.com>
2016-12-18 13:25:35 +0200

    simple dialogFragment added

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -1,13 +1,25 @@
 package com.nanddgroup.itea_app;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bShowDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bShowDialog = (Button) findViewById(R.id.bShowDialog);
+        bShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                TestDialogFragment.newInstance(null).show(ft, "Some_tag");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nanddgroup/itea_app/TestDialogFragment.java
@@ -0,0 +1,53 @@
+package com.nanddgroup.itea_app;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TestDialogFragment extends DialogFragment {
+
+
+    public TestDialogFragment() {
+        // Required empty public constructor
+    }
+
+    public static TestDialogFragment newInstance(Bundle bundle){
+        TestDialogFragment testDialogFragment = new TestDialogFragment();
+        testDialogFragment.setArguments(bundle);
+        return testDialogFragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.e("dialog", "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_test_dialog, container, false);
+        Button etTextText = (Button) view.findViewById(R.id.bTest);
+        etTextText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "ButtonClicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        Log.e("dialog", "onCreate");
+    }
+}
