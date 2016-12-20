52c97c9e2eb61c53078a46cb46fc610247d53bf0
Nikita Romanenko <nikita241296@gmail.com>
2016-12-20 19:57:37 +0200

    alert dialog full

--- app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
+++ app/src/main/java/com/nanddgroup/itea_app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import butterknife.ButterKnife;
@@ -11,11 +12,25 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DialogInterface.OnMultiChoiceClickListener onMultiChoiceClickListener;
+    private boolean[] checkedItems;
+    private String[] listData;
+    private ArrayAdapter<String> ad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        listData = new String[]{"Киев", "Львов", "Одесса", "Харьков"};
+        checkedItems = new boolean[]{false, true, false, false};
+        onMultiChoiceClickListener = new DialogInterface.OnMultiChoiceClickListener() {
+           @Override
+           public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+           }
+       };
+        ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
     }
 
     @OnClick(R.id.bAlertDialog)
@@ -43,8 +58,15 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setIcon(R.drawable.error_dialog)
                 .setTitle("Test Alert dialog")
+//                .setMultiChoiceItems(listData, checkedItems, onMultiChoiceClickListener)
+                .setAdapter(ad, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, String.valueOf(which), Toast.LENGTH_SHORT).show();
+                    }
+                })
                 .setView(R.layout.test_view_alert_dialog)
-                .setMessage("My message")
+//                .setMessage("My message")
                 .create().show();
     }
 }
